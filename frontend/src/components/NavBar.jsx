26745e2fe6280968855d015df5ff632174c1e26a
yakoubi rayen <rayen08yako@gmail.com>
2023-11-14 18:36:05 +0100

    create navbar

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import {Link} from 'react-router-dom'
+import Home from '../Pages/Home'
+
+const NavBar = () => {
+  return (
+    <div className='bg-red-500'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <h1 className='font-bold'>Bou7</h1>
+        <ul className='flex gap-4'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/sign-up'>Sign-up</Link></li>
+            <li><Link to='/sign-in'>Sign-in</Link></li>
+            <li><Link to='/profile'>Profile</Link></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
