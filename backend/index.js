e831c3f1c4cb13b8a8ea3dec75b2b4b39ef377e3
yakoubi rayen <rayen08yako@gmail.com>
2023-11-15 05:00:29 +0100

    hashing password signup route

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js'
+import authRoute from './routes/authRoute.js'
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 //console.log('Port:', port);
 
 const mongodbUri = process.env.MDB;
@@ -24,8 +25,11 @@ connectToDatabase();
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Connected to port: ${port}`);
 });
 
 app.use('/api/user', userRoutes );
+app.use('/api/auth', authRoute );
