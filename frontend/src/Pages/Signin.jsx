46031057feb10d5681ed60aad6a044c8ce934283
yakoubi rayen <rayen08yako@gmail.com>
2023-11-15 05:40:21 +0100

    some frontend + some backend

--- frontend/src/Pages/Signin.jsx
+++ frontend/src/Pages/Signin.jsx
@@ -2,6 +2,32 @@ import React from 'react'
 
 export default function Signin() {
   return (
-    <div className='flex justify-center mt-52 text-center'>Signin</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form  className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='Email'
+          id='email'
+          className='bg-slate-100 p-3 rounded-lg'
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          id='password'
+          className='bg-slate-100 p-3 rounded-lg'
+          
+        />
+        <button
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >submit
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont Have an account?</p>
+      </div>
+      <p className='text-red-700 mt-5'>
+      </p>
+    </div>
   )
 }
